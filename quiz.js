3ebc20a9f2e3214ad4e58844161cc0b60f5cb510
tillzmh <tillzmh@users.noreply.github.com>
2022-07-06 22:09:33 -0400

    global variables set

--- quiz.js
+++ quiz.js
@@ -2,5 +2,11 @@
   const timePerQuestion = 10; 
   const wrongAnswerPenalty = 5; 
   const rightAnswerReward = 10; 
-const storageName = "quiz";
+  const storageName = "quiz";
   const maxNumberOfHighScores = 10;
+
+  let score = 0;
+  let currentQuestionIndex = 0;
+  let timeRemaining;
+  let timer;
+  let initials = "";
